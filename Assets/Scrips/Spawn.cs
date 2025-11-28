d44157795bfbe554a8e8d8d0c3fea634d4f830c2
Kime-MangaX <kimemanga@gmail.com>
2025-11-28 10:42:21 -0500

    Update Spawn.cs

--- Assets/Scrips/Spawn.cs
+++ Assets/Scrips/Spawn.cs
@@ -2,13 +2,38 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    public GameObject player;
+    public int enemyRango;
+    public float generatecFrec;
+
+    float currentTime;
     void Start()
     {
-        
+       
     }
 
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+        print(currentTime);
+        if (currentTime >= generatecFrec)
+        {
+            GenerateEnemy();
+            currentTime = 0;
+        }
+    }
+
+    public void GenerateEnemy()
+    {
+        float X = Random.Range(-1f, 1f);
+        float Y = Random.Range(-1f, 1f);
+        Vector2 enemypost = new Vector2(X, Y);
+        enemypost = enemypost.normalized;
+        Vector2 playerPost = player.transform.position;
+        GameObject Enemy = Instantiate(enemyPrefab);
+        Enemy.transform.position = (enemypost * enemyRango) + playerPost;
+        print(playerPost);
+        print(enemypost);
     }
 }
